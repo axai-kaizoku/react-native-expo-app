b9afec700d8d8d6723e92a3d57be127ec6bf66fd
axai-kaizoku <02b3akshay@gmail.com>
2024-06-07 18:15:18 +0530

    add homepage

--- app/index.tsx
+++ app/index.tsx
@@ -13,15 +13,24 @@ export default function HomePage() {
 	return (
 		<SafeAreaView className="h-full bg-slate-800">
 			<ScrollView contentContainerStyle={{ height: '100%' }}>
-				<View className="items-center justify-center w-full h-full px-4 ">
+				<View className="items-center w-full h-full px-4 ">
 					<Image
-						source={images.logo as unknown as ImageSourcePropType}
+						source={images.logo as ImageSourcePropType}
 						className="w-32 h-20"
 						resizeMode="contain"
 					/>
 					<Image
-						source={images.cards as unknown as ImageSourcePropType}
+						source={images.cards as ImageSourcePropType}
 						className="max-w-xs w-full h-[300px]"
+						resizeMode="contain"
+					/>
+					<Text className="text-3xl font-bold text-white">
+						Discover endless possibilities with{' '}
+						<Text className="text-3xl text-orange-400/90">Aora</Text>
+					</Text>
+					<Image
+						source={images.path as ImageSourcePropType}
+						className="h-4 w-28 -right-32 -top-2"
 						resizeMode="contain"
 					/>
 				</View>
