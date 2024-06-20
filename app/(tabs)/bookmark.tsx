caf1a38e780cd6b12324e814c4b7c7468505df10
axai-kaizoku <02b3akshay@gmail.com>
2024-06-20 16:20:58 +0530

    add updated bookmark

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Bookmark() {
 	return (
 		<View className="items-center justify-center w-full h-full bg-slate-900">
-			<Text className="text-xl font-semibold text-white">
+			<Text className="text-xl font-semibold text-white ">
 				Coming Soon 🖕(◔◡◔)
 			</Text>
 		</View>
